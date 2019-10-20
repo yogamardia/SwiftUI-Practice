cf876bd66225df4eb9d36bddb7cae438b8eec247
yogamardia <yoga.mardia@igloohome.co>
2019-10-20 10:23:08 +0800

    restructured project structure

---
+++ SwiftUI-Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Practice
+//
+//  Created by Yoga Mardia on 20/10/19.
+//  Copyright © 2019 Yoga Mardia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
